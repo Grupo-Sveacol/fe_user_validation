f91b99b94d1df4d937796056f97a52c147862667
CeyniPBH <cpbeltranh@eafit.edu.co>
2025-04-23 22:02:42 -0500

    Create test cases files.

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,30 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
 
-export default [
-  { ignores: ['dist'] },
+export default defineConfig([
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js, react: pluginReact },
+    extends: ["js/recommended"],
     languageOptions: {
-      ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
       },
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
     rules: {
-      ...js.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
     },
   },
-]
+  pluginReact.configs.flat.recommended,
+]);
---
+++ src/test/kycVerification.test.js
@@ -0,0 +1,35 @@
+import { Builder, By, until } from "selenium-webdriver";
+
+(async function verifyKYCButton() {
+  let driver = await new Builder().forBrowser("chrome").build();
+
+  try {
+    // Navegar a la URL inicial
+    await driver.get("https://pagui-kyc.vercel.app");
+
+    // Esperar a que el botón esté presente
+    const startButton = await driver.wait(
+      until.elementLocated(By.css("button[aria-label='Next']")),
+      5000 // Esperar hasta 5 segundos
+    );
+
+    // Verificar que el botón sea visible
+    const isDisplayed = await startButton.isDisplayed();
+    console.log("Start Verification button is visible:", isDisplayed);
+
+    // Hacer clic en el botón
+    await startButton.click();
+
+    // Depurar la URL actual después de hacer clic
+    const currentUrl = await driver.getCurrentUrl();
+    console.log("Current URL after clicking the button:", currentUrl);
+
+    // Verificar que la URL cambie al siguiente paso
+    await driver.wait(until.urlContains("/terms"), 10000); // Aumentar tiempo de espera
+    console.log("Redirection to KYC step 1 successful!");
+  } catch (error) {
+    console.error("Test failed:", error);
+  } finally {
+    await driver.quit();
+  }
+})();
